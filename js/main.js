56f8d9c9c55c66a76a8cee2d7387f38381713352
roman <19roman23@gmail.com>
2020-01-04 21:32:30 +0200

    create slider for intro block

--- js/main.js
+++ js/main.js
@@ -9,5 +9,16 @@ $(function(){
     navToggle.on("click", function(event){
         event.preventDefault();
         $(".burger__icon").toggleClass("show");
-    })
+    });
+    /*slick slider*/
+    const dataSlider = $('[data-slider="intro-slider"]');
+    dataSlider.slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        fade: true,
+        arrows: false,
+        dots: true,
+        autoplay: true
+      });
 });
