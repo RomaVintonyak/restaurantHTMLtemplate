500bc8858d6176956c53a1f85d1b01cb585fbcf4
RomaVintonyak <19roman23@gmail.com>
2020-01-04 15:44:21 +0200

    create butger button

--- js/main.js
+++ js/main.js
@@ -4,4 +4,10 @@ $(function(){
         event.preventDefault();
         $(this).toggleClass("nav__link--active");
     });
+    /*toggle class show to burger button*/
+    var navToggle = $("#navToggle");
+    navToggle.on("click", function(event){
+        event.preventDefault();
+        $(".burger__icon").toggleClass("show");
+    })
 });
