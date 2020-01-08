b0ce32655299616bb79a1a6ebbb115516a081881
RomaVintonyak <19roman23@gmail.com>
2020-01-08 13:43:43 +0200

    create features slider

--- js/main.js
+++ js/main.js
@@ -1,27 +1,36 @@
-$(function(){
-    /*toogle class active to nav link*/
-    $(".nav__link").on("click", function(event){
-        event.preventDefault();
-        $(this).toggleClass("nav__link--active");
-    });
-    /*toggle class show to burger button*/
-    var navToggle = $("#navToggle");
-    navToggle.on("click", function(event){
-        event.preventDefault();
-        $(".burger__icon").toggleClass("show");
-    });
-    /*slick slider*/
-    const dataSlider = $('[data-slider="intro-slider"]');
-    dataSlider.slick({
-        infinite: true,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        fade: true,
-        arrows: false,
-        dots: true,
-        autoplay: true
-      });
-      /*waves evect*/
-      Waves.attach(".wavesEfect");
-      Waves.init();
+$(function() {
+  /*toogle class active to nav link*/
+  $(".nav__link").on("click", function(event) {
+    event.preventDefault();
+    $(this).toggleClass("nav__link--active");
+  });
+  /*toggle class show to burger button*/
+  var navToggle = $("#navToggle");
+  navToggle.on("click", function(event) {
+    event.preventDefault();
+    $(".burger__icon").toggleClass("show");
+  });
+  /*slick slider*/
+  const dataSlider = $('[data-slider="intro-slider"]');
+  dataSlider.slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    fade: true,
+    arrows: false,
+    dots: true,
+    autoplay: true
+  });
+  /*waves evect*/
+  Waves.attach(".wavesEfect");
+  Waves.init();
+  /*features slider*/
+  const featureSlid = $("#featuresSlider");
+  featureSlid.slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    arrows: false
+  });
 });
